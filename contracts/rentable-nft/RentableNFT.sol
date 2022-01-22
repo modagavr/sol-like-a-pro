c15f94ac9d52e8e7bcfd774bbc8eb6e7bb78382f
Egor Gavrilov <modagavr@gmail.com>
2022-01-22 18:34:39 +0300

    💥Rentable NFT

---
+++ contracts/rentable-nft/RentableNFT.sol
@@ -0,0 +1,101 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract RentableNFT is ERC721, ERC721Enumerable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    struct Rental {
+        bool isActive;
+        address lord;
+        address renter;
+        uint256 expiresAt;
+    }
+
+    mapping(uint256 => Rental) public rental;
+
+    event Rented(
+        uint256 indexed tokenId,
+        address indexed lord,
+        address indexed renter,
+        uint256 expiresAt
+    );
+
+    event FinishedRent(
+        uint256 indexed tokenId,
+        address indexed lord,
+        address indexed renter,
+        uint256 expiresAt
+    );
+
+    constructor() ERC721("RentableNFT", "RENT") {}
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    function rentOut(
+        address renter,
+        uint256 tokenId,
+        uint256 expiresAt
+    ) external {
+        _transfer(msg.sender, renter, tokenId);
+
+        rental[tokenId] = Rental({
+            isActive: true,
+            lord: msg.sender,
+            renter: renter,
+            expiresAt: expiresAt
+        });
+
+        emit Rented(tokenId, msg.sender, renter, expiresAt);
+    }
+
+    function finishRenting(uint256 tokenId) external {
+        Rental storage _rental = rental[tokenId];
+
+        require(
+            msg.sender == _rental.renter ||
+                block.timestamp >= _rental.expiresAt,
+            "RentableNFT: this token is rented"
+        );
+
+        _rental.isActive = false;
+
+        _transfer(_rental.renter, _rental.lord, tokenId);
+
+        emit FinishedRent(
+            tokenId,
+            _rental.lord,
+            _rental.renter,
+            _rental.expiresAt
+        );
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        require(!rental[tokenId].isActive, "RentableNFT: this token is rented");
+
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ test/rentable-nft/RentableNFT.test.ts
@@ -0,0 +1,147 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { expect } from 'chai'
+import dayjs from 'dayjs'
+import { BigNumber, constants } from 'ethers'
+import { ethers, network } from 'hardhat'
+
+import { RentableNFT, RentableNFT__factory } from '../../typechain-types'
+
+describe('Rentable NFT', () => {
+  let owner: SignerWithAddress,
+    lord: SignerWithAddress,
+    renter: SignerWithAddress,
+    guy: SignerWithAddress
+
+  let token: RentableNFT
+
+  beforeEach(async () => {
+    const signers = await ethers.getSigners()
+
+    owner = signers[0]
+
+    lord = signers[1]
+
+    renter = signers[2]
+
+    guy = signers[3]
+
+    token = await new RentableNFT__factory(owner).deploy()
+  })
+
+  it('Mint & Transfer', async () => {
+    expect(await token.totalSupply()).to.eql(constants.Zero)
+
+    await token.safeMint(lord.address)
+
+    expect(await token.totalSupply())
+      .to.eql(await token.balanceOf(lord.address))
+      .to.eql(constants.One)
+
+    expect(
+      await token.connect(lord).transferFrom(lord.address, renter.address, 0)
+    )
+
+    expect(await token.totalSupply())
+      .to.eql(await token.balanceOf(renter.address))
+      .to.eql(constants.One)
+
+    expect(await token.balanceOf(lord.address)).to.eql(constants.Zero)
+  })
+
+  describe('Rent Out & Finish Renting', () => {
+    const expiresAt = dayjs().add(1, 'day').unix()
+
+    beforeEach(async () => {
+      await token.safeMint(lord.address)
+      await token.safeMint(lord.address)
+      await token.safeMint(lord.address)
+
+      expect(await token.totalSupply())
+        .to.eql(await token.balanceOf(lord.address))
+        .to.eql(BigNumber.from(3))
+
+      await expect(
+        token.rentOut(renter.address, 1, expiresAt)
+      ).to.be.revertedWith('ERC721: transfer of token that is not own')
+
+      await expect(token.connect(lord).rentOut(renter.address, 1, expiresAt))
+        .to.emit(token, 'Rented')
+        .withArgs(1, lord.address, renter.address, expiresAt)
+
+      expect(
+        await Promise.all([
+          token.totalSupply(),
+          token.balanceOf(lord.address),
+          token.balanceOf(renter.address),
+          token.ownerOf(1)
+        ])
+      ).to.eql([
+        BigNumber.from(3),
+        constants.Two,
+        constants.One,
+        renter.address
+      ])
+
+      const rental = await token.rental(1)
+
+      expect([
+        rental.isActive,
+        rental.lord,
+        rental.renter,
+        rental.expiresAt
+      ]).to.eql([true, lord.address, renter.address, BigNumber.from(expiresAt)])
+
+      await expect(
+        token.connect(renter).transferFrom(renter.address, guy.address, 1)
+      ).to.be.revertedWith('RentableNFT: this token is rented')
+
+      await expect(token.finishRenting(1)).to.be.revertedWith(
+        'RentableNFT: this token is rented'
+      )
+    })
+
+    it('Early Finish', async () => {
+      await expect(token.connect(renter).finishRenting(1))
+        .to.emit(token, 'FinishedRent')
+        .withArgs(1, lord.address, renter.address, expiresAt)
+    })
+
+    it('After Expiration', async () => {
+      await network.provider.send('evm_setNextBlockTimestamp', [expiresAt])
+
+      await expect(token.connect(guy).finishRenting(1))
+        .to.emit(token, 'FinishedRent')
+        .withArgs(1, lord.address, renter.address, expiresAt)
+    })
+
+    afterEach(async () => {
+      expect(
+        await Promise.all([
+          token.totalSupply(),
+          token.balanceOf(lord.address),
+          token.balanceOf(renter.address),
+          token.ownerOf(1)
+        ])
+      ).to.eql([
+        BigNumber.from(3),
+        BigNumber.from(3),
+        constants.Zero,
+        lord.address
+      ])
+
+      const rental = await token.rental(1)
+
+      expect([
+        rental.isActive,
+        rental.lord,
+        rental.renter,
+        rental.expiresAt
+      ]).to.eql([
+        false,
+        lord.address,
+        renter.address,
+        BigNumber.from(expiresAt)
+      ])
+    })
+  })
+})
